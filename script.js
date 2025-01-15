8ce280b50837ae577f4937bf3de396f1a0a864b9
Samiksha <agarwalsamiksha88@gmail.com>
2025-01-15 22:12:35 +0530

    Made the calculator to function

--- script.js
+++ script.js
@@ -1,85 +1,102 @@
 let innerHtml = '';
 
-function add(a, b){
+function operate(string){
+    let operand1 = '', operand2 = '';
+    let operator;
+    let ind = 0;
+    let i;
+    for(i = 0; i < string.length; i++){
+        let item = string.charAt(i);
+        if(item === '+' || item === '-' || item ==='x' || item === '÷'){
+            ind = i;
+            break;
+        }
+    }
+    for(i = 0; i < ind; i++){
+        operand1 += string.charAt(i);
+    }
+    if(operand1){
+        operand1 = parseFloat(operand1);
+    }
+    for(i = ind; i < string.length; i++){
+        let temp = string.charAt(i);
+        if(temp >= '0' && temp <= '9'){
+            operand2 += temp;
+        }
+        else{
+            if(operator){
+                operand2 = parseFloat(operand2);
+                if(operator === '+')
+                    operand1 = add(operand1, operand2);
+                else if(operator === '-')
+                    operand1 = subtract(operand1, operand2);
+                else if(operator === 'x')
+                    operand1 = multiply(operand1, operand2);
+                else{
+                    if(operand2 === 0)
+                        return 'Error';
+                    operand1 = divide(operand1, operand2);
+                }
+            }
+            operator = temp;
+            operand2 = '';
+        }
+    }
+    if(operator){
+        operand2 = parseFloat(operand2);
+        if(operator === '+')
+            operand1 = add(operand1, operand2);
+        else if(operator === '-')
+            operand1 = subtract(operand1, operand2);
+        else if(operator === 'x')
+            operand1 = multiply(operand1, operand2);
+        else{
+            if(operand2 === 0)
+                return 'Error';
+            operand1 = divide(operand1, operand2);
+        }
+    }
+
+    if(operand1){
+        if(operand1 % 1 !== 0 && operand1.toString().split('.')[1].length > 6)
+            operand1 = operand1.toFixed(6);
+        return operand1;
+    }
+    return 'Error';
+}
+
+
+
+// Example mathematical operations
+function add(a, b) {
     return a + b;
 }
 
-function subtract(a, b){
+function subtract(a, b) {
     return a - b;
 }
 
-function multiply(a, b){
+function multiply(a, b) {
     return a * b;
 }
 
-function divide(a, b){
-    return a / b;
+function divide(a, b) {
+    return b === 0 ? 'Error' : a / b;
 }
-
-function operate(innerHtml) {
-    let i, res = null, num = 0, operator = null;
-    for (i = 0; i < innerHtml.length; i++) {
-        let c = innerHtml.charAt(i);
-        if (c >= '0' && c <= '9') {
-            num = num * 10 + parseInt(c);
-        } else {
-            if (operator) {
-                switch (operator) {
-                    case '+':
-                        res = add(res, num);
-                        break;
-                    case '-':
-                        res = subtract(res, num);
-                        break;
-                    case 'x':
-                        res = multiply(res, num);
-                        break;
-                    case '÷':
-                        res = divide(res, num);
-                        break;
-                }
-            } else {
-                res = num;
-            }
-            operator = c;
-            num = 0;
-        }
-    }
-    // Perform the last operation
-    if (operator) {
-        switch (operator) {
-            case '+':
-                res = add(res, num);
-                break;
-            case '-':
-                res = subtract(res, num);
-                break;
-            case 'x':
-                res = multiply(res, num);
-                break;
-            case '÷':
-                res = divide(res, num);
-                break;
-        }
-    }
-    innerHtml = res;
-    document.querySelector('.display').innerHTML = innerHtml;
-    return innerHtml;
-}
-
 
 document.querySelectorAll('button').forEach((item) => {
     item.addEventListener("click", () => {
-        if(item.innerHTML !== '=' && item.innerHTML !== 'Clear'){
-            innerHtml += item.innerHTML;
+        if(item.innerHTML === '='){
+            innerHtml = operate(innerHtml);
             document.querySelector('.display').innerHTML = innerHtml;
         }
         else if(item.innerHTML === 'Clear'){
             innerHtml = '';
-            document.querySelector('.display').innerHTML = innerHtml;
+            document.querySelector('.display').innerHTML = '';
         }
         else{
-            innerHtml = operate(innerHtml);
+            innerHtml += item.innerHTML;
+            document.querySelector('.display').innerHTML = innerHtml;
         }
     })
-});
+})
