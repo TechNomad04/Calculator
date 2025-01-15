260c7a5acca62dfc59e9fda70dc7d8662beb7f8d
Samiksha <agarwalsamiksha88@gmail.com>
2025-01-15 12:44:23 +0530

    Updated the display

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let operand1, operator, operand2;
+let innerHtml = '';
 
 function add(a, b){
     return a + b;
@@ -16,13 +16,70 @@ function divide(a, b){
     return a / b;
 }
 
-function operate(a, operator, b){
-    if(operator === '+')
-        add(a, b);
-    else if (operator === '-')
-        subtract(a, b);
-    else if(operator === '*')
-        multiply(a, b);
-    else if(operator === '/')
-        divide(a, b);
+function operate(innerHtml) {
+    let i, res = null, num = 0, operator = null;
+    for (i = 0; i < innerHtml.length; i++) {
+        let c = innerHtml.charAt(i);
+        if (c >= '0' && c <= '9') {
+            num = num * 10 + parseInt(c);
+        } else {
+            if (operator) {
+                switch (operator) {
+                    case '+':
+                        res = add(res, num);
+                        break;
+                    case '-':
+                        res = subtract(res, num);
+                        break;
+                    case 'x':
+                        res = multiply(res, num);
+                        break;
+                    case '÷':
+                        res = divide(res, num);
+                        break;
+                }
+            } else {
+                res = num;
+            }
+            operator = c;
+            num = 0;
+        }
+    }
+    // Perform the last operation
+    if (operator) {
+        switch (operator) {
+            case '+':
+                res = add(res, num);
+                break;
+            case '-':
+                res = subtract(res, num);
+                break;
+            case 'x':
+                res = multiply(res, num);
+                break;
+            case '÷':
+                res = divide(res, num);
+                break;
+        }
+    }
+    innerHtml = res;
+    document.querySelector('.display').innerHTML = innerHtml;
+    return innerHtml;
 }
+
+
+document.querySelectorAll('button').forEach((item) => {
+    item.addEventListener("click", () => {
+        if(item.innerHTML !== '=' && item.innerHTML !== 'Clear'){
+            innerHtml += item.innerHTML;
+            document.querySelector('.display').innerHTML = innerHtml;
+        }
+        else if(item.innerHTML === 'Clear'){
+            innerHtml = '';
+            document.querySelector('.display').innerHTML = innerHtml;
+        }
+        else{
+            innerHtml = operate(innerHtml);
+        }
+    })
+});
